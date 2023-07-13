33be360aa0fce00f75673065e79d87ca0076b7ef
Danila Petrov <dp200308@gmail.com>
2023-07-13 19:51:19 +0500

    disabled user select for header

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className='sticky top-0 z-50 flex h-20 flex-shrink-0 items-center bg-grays'>
       <Container className='flex items-center justify-center'>
-        <h1 className='text-5xl font-bold text-white'>Навигатор</h1>
+        <h1 className='text-5xl font-bold text-white select-none'>Навигатор</h1>
       </Container>
     </header>
   );
